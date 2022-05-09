179111450092971a50becdb737dcd47c7a790c67
xfzheng <xfzheng@hillinsight.com>
2022-05-09 14:26:47 +0800

    静态库

---
+++ app/app.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+# CreateDate: 2022/5/9 11:37
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import gunicorn.app.base
+
+from flask import Flask
+from watermarkso import WaterMark
+import orjson
+
+app = Flask(__name__)
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super(StandaloneApplication, self).__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
+@app.route('/add_water_mark', methods=['GET', 'POST'])
+def route_api():
+    wm = WaterMark()
+    water_file = wm.add_water_mask("../abc.pdf", "随意 水印 水印 索引")
+    wm.free()
+    with open("out-water.pdf", "wb") as fw:
+        fw.write(water_file)
+    return orjson.dumps({"code":0, "msg":"ok"})
+    
+if __name__ == '__main__':
+    # app.run(host="127.0.0.1",port=5000,debug=True)
+    StandaloneApplication(app, {
+        'bind': "127.0.0.1:5000",
+        'workers': 1,
+        'worker_class':'sync',
+        'timeout': 60,
+        'loglevel': 'info',
+        'logger_class': 'simple'
+    }).run()
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# CreateDate: 2022/5/9 14:17
+
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+# 这里我们不能在 sources 里面写上 ["fib.pyx", "libfib.a"]，这是不合法的，因为 sources 里面需要放入源文件
+# 静态库和动态库需要通过 library_dirs 和 libraries 指定
+ext = Extension(name="wrapper_watermark",
+                sources=["libwatermark.a"],
+                # 相当于 gcc 的 -L 参数，路径可以指定多个
+                library_dirs=["."],
+                # 相当于 gcc 的 -l 参数，链接的库可以指定多个
+                # 注意：不能写 libfib.a，直接写 fib 就行，所以静态命名需要遵循规范，要以 lib 开头、.a 结尾
+                # 动态库同理，lib 开头、.so 结尾
+                libraries=["watermark"]
+                # 如果还需要头文件的话，那么通过 include_dirs 指定
+                # 只不过由于头文件就在当前目录中，所以我们不需要指定
+                )
+
+setup(ext_modules=cythonize(ext, language_level=3))
