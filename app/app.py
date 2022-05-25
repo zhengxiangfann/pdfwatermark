35f5029a6824c2c437409d73aba3d154a07c9716
xfzheng <xfzheng@hillinsight.com>
2022-05-25 12:44:05 +0800

    fmt

--- app/app.py
+++ app/app.py
@@ -1,15 +1,20 @@
 # -*- coding:utf-8 -*-
+import base64
 import logging
-
-import orjson.orjson
 import time
+
+import orjson
+
 logging.basicConfig(level=logging.DEBUG)
 import gunicorn.app.base
 from flask import Flask, send_file
 # import orjson
 from io import BytesIO
-# import watermark as wm
-from watermarkso import WaterMark
+import wrapper_watermark as wm
+
+# from watermarkso import WaterMark
+# import gevent.monkey
+# gevent.monkey.patch_all()
 
 app = Flask(__name__)
 
@@ -20,11 +25,12 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
         self.application = app
         super(StandaloneApplication, self).__init__()
 
-    def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
+
+def load_config(self):
+    config = {key: value for key, value in self.options.items()
+              if key in self.cfg.settings and value is not None}
+    for key, value in config.items():
+        self.cfg.set(key.lower(), value)
 
     def load(self):
         return self.application
@@ -37,18 +43,23 @@ def index():
 
 @app.route('/add_water_mark', methods=['GET', 'POST'])
 def route_api():
-
-    params_file_path = "../libwatermark/abc.pdf"
-    params_content = "斗破苍穹 水印 水印 斗罗大陆"
+    params_file_path = "../libwatermark/abc.pdf".encode('utf-8')
+    params_content = "斗破苍穹 水印 水印 斗罗大陆".encode('utf-8')
 
     ts = time.time()
-    with_watermark_file = WaterMark().add_water_mask(params_file_path, params_content)
+    # with_watermark_file = WaterMark().add_water_mask(params_file_path, params_content)
+    with_watermark_file = wm.add_water_mark(params_file_path, params_content)
+    wm.release_memory()
 
     print("文件加水印耗时:", time.time() - ts)
+    print('watermark_file', len(with_watermark_file))
 
     filename = "water_mark-abc.pdf"
+    with open(filename, "wb") as fw:
+        fw.write(with_watermark_file)
+
     response = send_file(
-        BytesIO(with_watermark_file),
+        BytesIO(base64.b64decode(with_watermark_file)),
         as_attachment=False,
         attachment_filename=filename,
         cache_timeout=1
@@ -59,12 +70,12 @@ def route_api():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
-    # StandaloneApplication(app, {
-    #     'bind': "127.0.0.1:5000",
-    #     'workers': 1,
-    #     'worker_class':'sync',
-    #     'timeout': 60,
-    #     'loglevel': 'info',
-    #     'logger_class': 'simple'
-    # }).run()
+    # app.run(host="127.0.0.1", port=5000, debug=True)
+    StandaloneApplication(app, {
+        'bind': "127.0.0.1:5000",
+        'workers': 1,
+        'worker_class': 'sync',
+        'timeout': 60,
+        'loglevel': 'info',
+        'logger_class': 'simple'
+    }).run()
